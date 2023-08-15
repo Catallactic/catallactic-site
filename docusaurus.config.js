f4d762808d98c006f5ea24332bac62ee5265f6ce
pellyadolfo <pellyadolfo@yahoo.es>
2023-08-15 17:11:20 +0200

    do not use jekyll

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://catallactic.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/catallactic-site',
+  baseUrl: '/catallactic-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
