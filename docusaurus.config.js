43205b8f2c02c1f60c242a533eac61d6d5e51bea
pellyadolfo <pellyadolfo@yahoo.es>
2023-09-22 15:33:55 +0200

    updated docusaurus version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,22 +41,16 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Catallactic/catallactic-site/blob/main',
+          editUrl: 'https://github.com/Catallactic/catallactic-site/blob/main',
         },
         blog: {
           blogSidebarTitle: 'All posts',
           blogSidebarCount: 'ALL',
 					showReadingTime: true,
 					sortPosts: 'ascending',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Catallactic/catallactic-site/blob/main',
+          editUrl: 'https://github.com/Catallactic/catallactic-site/blob/main',
         },
-        theme: {
+				theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
