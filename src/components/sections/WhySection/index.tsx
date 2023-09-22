b701278db4b9f74787bd3edd94d3db6ca6e98cab
pellyadolfo <pellyadolfo@yahoo.es>
2023-09-22 11:29:22 +0200

    updated landing faqs

--- src/components/sections/WhySection/index.tsx
+++ src/components/sections/WhySection/index.tsx
@@ -7,15 +7,20 @@ import styles from './styles.module.css';
 function setShownTab(num: number) {
 	document.getElementById("whytab0").classList.remove("show");
 	document.getElementById("whytab0").classList.remove("active");
+	document.getElementById("v-whytab0").classList.remove("active");
 	document.getElementById("whytab1").classList.remove("show");
 	document.getElementById("whytab1").classList.remove("active");
+	document.getElementById("v-whytab1").classList.remove("active");
 	document.getElementById("whytab2").classList.remove("show");
 	document.getElementById("whytab2").classList.remove("active");
+	document.getElementById("v-whytab2").classList.remove("active");
 	document.getElementById("whytab3").classList.remove("show");
 	document.getElementById("whytab3").classList.remove("active");
+	document.getElementById("v-whytab3").classList.remove("active");
 	
 	document.getElementById("whytab" + num).classList.add("show");
 	document.getElementById("whytab" + num).classList.add("active");
+	document.getElementById("v-whytab" + num).classList.add("active");
 }
 
 export default function WhySection(): JSX.Element {
@@ -32,19 +37,19 @@ export default function WhySection(): JSX.Element {
             <div className="col-md-3">
 							<div className="nav flex-column nav-pills nav-pills-custom" id="v-pills-tab" role="tablist" aria-orientation="vertical">
 
-								<a className="nav-link mb-3 p-3 shadow active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true" onClick={() => setShownTab(0)}>
+								<a className="pp nav-link mb-3 p-3 shadow active" id="v-whytab0" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true" onClick={() => setShownTab(0)}>
 									<span className="font-weight-bold small text-uppercase">Why Catallactic</span>
 								</a>
 
-								<a className="nav-link mb-3 p-3 shadow" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false" onClick={() => setShownTab(1)}>
-									<span className="font-weight-bold small text-uppercase">Why is no a stablecoin?</span>
+								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab1" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false" onClick={() => setShownTab(1)}>
+									<span className="font-weight-bold small text-uppercase">Why not stablecoins?</span>
 								</a>
 
-								<a className="nav-link mb-3 p-3 shadow" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false" onClick={() => setShownTab(2)}>
+								<a className="nav-link mb-3 p-3 shadow" id="v-whytab2" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false" onClick={() => setShownTab(2)}>
 									<span className="font-weight-bold small text-uppercase">How can I benefit</span>
 								</a>
 
-								<a className="nav-link mb-3 p-3 shadow" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false" onClick={() => setShownTab(3)}>
+								<a className="nav-link mb-3 p-3 shadow" id="v-whytab3" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false" onClick={() => setShownTab(3)}>
 									<span className="font-weight-bold small text-uppercase">How can I contribute</span>
 								</a>
 
@@ -61,14 +66,14 @@ export default function WhySection(): JSX.Element {
 									</div>
 									
 									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab1" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-										<h4 className="font-italic mb-4">Why is not an stablecoin</h4>
-										<p className="font-italic text-muted mb-2">Crypto was born in 2013 with the promise of decentralization from a non fiat currency, Bitcoin. However, tokenization projects have evolved with the financing of venture-capital backed crypto companies and financial firms, that have modified the initial promise, to make it more centralized and fiat dependent. Whereas we think this is a distortion from the initial principles, we find that the tools and processs developed inherit this centraliced and fiat nature and alternative approaches are miss-represented. In short there is a fiat bias in tools and processes.</p>
+										<h4 className="font-italic mb-4">Why not stablecoins?</h4>
+										<p className="font-italic text-muted mb-2">Crypto was born in 2013 with the promise of decentralization from a non fiat currency, Bitcoin. However, tokenization projects have evolved with the financing of venture-capital backed crypto companies and financial firms, that have modified the initial promise, to make it more centralized and fiat dependent. Whereas we think this is a distortion from the initial principles, we find that the tools and processs developed inherit this centraliced and fiat nature and alternative approaches are miss-represented. In short there is a fiat bias in tools and processes. Stablecoins are centralized by definition..</p>
 										<p className="font-italic text-muted mb-2">Catallactic aims to define the principles, know-how, tools and processes to tokenize, fund and manage decentralized commodity based tokens and relief the issuer from the technological and operational burden.</p>
 									</div>
 									
 									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab2" role="tabpanel" aria-labelledby="v-pills-messages-tab">
 										<h4 className="font-italic mb-4">How can I benefit?</h4>
-										<p className="font-italic text-muted mb-2">Catallactic aims to relief cryptocurrency issuers of Units of Account of the burden to define principles, acquire know-how, develop tools and design processes to issue tokens backed by the value of their underlying assets. The goal is that cryptocurrency issuers can focus on their core business. The goal, for the sake of practicality is to create an opinionated system, but maintain configurability.</p>
+										<p className="font-italic text-muted mb-2">Catallactic aims to relief cryptocurrency issuers of Units of Account of the burden to define principles, acquire know-how, develop tools and design processes to issue tokens backed by the value of their underlying assets. The goal, for the sake of practicality is to create an opinionated system, but maintain configurability, which allows cryptocurrency issuers focusing on their core business.</p>
 										<p className="font-italic text-muted mb-2">The outcomes from Catallactic are Open Source and will allow private actors to build additional tools, plugins or know-how that can complement Catallactic deliverables.</p>
 									</div>
 									
