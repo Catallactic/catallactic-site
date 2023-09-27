4f4fde025f415fbdf59141296207c96c8f26f039
pellyadolfo <pellyadolfo@yahoo.es>
2023-09-27 23:01:37 +0200

    update styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,10 +34,10 @@ function HomepageHeader() {
 						</div>
 						<div className='col-12 col-md-4 text-white'>
 							<h1 className='mb-5'>Catallactic</h1>
-							<h2 className='mb-5'>Building a <b>real economy</b> backed by <b>commodities</b></h2>
+							<h2 className='mb-5'>Building a <b>Real Economy</b> backed by <b>Commodities</b></h2>
 							<h4 className='mb-5'>... because <i>'only fools confuse price and value'</i>. Antonio Machado.</h4>
-							<a className='btn btn-outline-light btn-lg m-3' href='https://github.com/Catallactic' role='button'>Access on Github</a>
-							<a className='btn btn-outline-light btn-lg m-3' href='https://discord.gg/xwJR9MZPmA' role='button'>Access on Discord</a>
+							<a className='btn btn-outline-light btn-lg m-3 actionbutton' href='https://github.com/Catallactic' role='button'>Access on Github</a>
+							<a className='btn btn-outline-light btn-lg m-3 actionbutton' href='https://discord.gg/xwJR9MZPmA' role='button'>Access on Discord</a>
 						</div>
 						<div className='d-none col-md-2'></div>
 					</div>
