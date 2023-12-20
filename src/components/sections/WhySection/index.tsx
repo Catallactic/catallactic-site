d82db97a8514bc8f39e38521100a8c0f2ccdd3c3
pellyadolfo <pellyadolfo@yahoo.es>
2023-12-20 14:58:37 +0100

    updated frontpage

--- src/components/sections/WhySection/index.tsx
+++ src/components/sections/WhySection/index.tsx
@@ -68,9 +68,9 @@ export default function WhySection(): JSX.Element {
 
 									<div className="tab-pane fade shadow rounded bg-white p-5 show active" id="whytab0" role="tabpanel" aria-labelledby="v-pills-home-tab">
 										<h4 className="font-italic mb-4">Why Catallactic?</h4>
-										<p className="font-italic text-muted mb-2">Catallactic is created to build cryptocurrencies capturing value of Commodities.</p>
+										<p className="font-italic text-muted mb-2">Because this is the real crypto. Catallactic is created to build cryptocurrencies capturing value of Commodities.</p>
 										<p className="font-italic text-muted mb-2">Capturing value means transferring to the token price the marginal utility as perceived by consumers. Differently to fiat stablecoins, where the price is captured from an external market, CryptoCommodities allow a natural price formation performed by a required underlying market of the commodity. This guarantees <b>no relative price distortion other than consumers will</b>.</p>
-										<p className="font-italic text-muted mb-2">Capturing the marginal utility of commodity provides to CryptoCommodities <a href="/blog/The%206%20Styles%20of%20RWA%20Tokenization#6-rwa-unit-of-account">different features to an stablecoin</a>.</p>
+										<p className="font-italic text-muted mb-2">Capturing the marginal utility of commodity provides to CryptoCommodities <a href="/docs/whitepaper#31-the-underlying-asset">different features to a stablecoin</a>.</p>
 										<p className="font-italic text-muted mb-2">We break with the Quantitative Theory of Money, which assumes a monopolistic legal tender and a central planner, to build a <b>Qualitative Theory of Money</b> for competing currencies privately issued.</p>
 									</div>
 									
