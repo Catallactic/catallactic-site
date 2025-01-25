9acfdae12d1afedf1afd6332e12688108cccf147
pellyadolfo <pellyadolfo@yahoo.es>
2025-01-25 19:59:24 +0100

    update frontpage

--- src/components/sections/WhySection/index.tsx
+++ src/components/sections/WhySection/index.tsx
@@ -74,9 +74,9 @@ export default function WhySection(): JSX.Element {
 									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab1" role="tabpanel" aria-labelledby="v-pills-competition-tab">
 										<h4 className="font-italic mb-4">Backed By Commodities</h4>
 										<hr/>
-										<p className="font-italic text-muted mb-2">Catallactic aims to build cryptocurrencies that comply with <p>Regression Theorem</p>, and therefore, that capture the intrinsic value of Commodities as underlaying asset. This is why the currencies are called as <p>CryptoCommodities</p>.</p>
-										<p className="font-italic text-muted mb-2">We <p>capture and expose the intrinsic value</p> of the underlying commodity by binding a unit of the commodity to a unit of the CryptoCommodity. This binding is enforced by the smart contact processing and users interaction.</p>
-										<p className="font-italic text-muted mb-2">Different Commodities, issuers and scopes will have different features, so there is a degree of <p>customization</p> for the dimensions of every CryptoCommodity.</p>
+										<p className="font-italic text-muted mb-2">Catallactic aims to build cryptocurrencies that comply with <b>Regression Theorem</b>, and therefore, that capture the intrinsic value of Commodities as underlaying asset. This is why the currencies are called as <b>CryptoCommodities</b>.</p>
+										<p className="font-italic text-muted mb-2">We <b>capture and expose the intrinsic value</b> of the underlying commodity by binding a unit of the commodity to a unit of the CryptoCommodity. This binding is enforced by the smart contact processing and users interaction.</p>
+										<p className="font-italic text-muted mb-2">Different Commodities, issuers and scopes will have different features, so there is a degree of <b>customization</b> for the dimensions of every CryptoCommodity.</p>
 									</div>
 
 									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab2" role="tabpanel" aria-labelledby="v-pills-backed-tab">
@@ -84,33 +84,33 @@ export default function WhySection(): JSX.Element {
 										<hr/>
 										<p className="font-italic text-muted mb-2">By exposing commodities intrinsic value enables capturing into the token price the <b>marginal utility as perceived by consumers</b>. Differently to fiat stablecoins, where the price is captured from an external market, CryptoCommodities allow a natural price formation performed by a required underlying market of the commodity. This guarantees <b>no relative price distortion other than consumers will</b>.</p>
 										<p className="font-italic text-muted mb-2">Commodities backed cryptocurrencies hold <a href="/docs/whitepaper#31-the-underlying-asset">different features to a stablecoin</a> and are eligible to participate into the price system of the adopting jurisdiction.</p>
-										<p className="font-italic text-muted mb-2"><b>Catallactic advocates for a <p>built-in price formation</p> as a basis to deliver true <p>value decentralization</p>. Stablecoins are centralized because they are pegged to a fiat price formed in international markets</b>.</p>
+										<p className="font-italic text-muted mb-2"><b>Catallactic advocates for a <b>built-in price formation</b> as a basis to deliver true <b>value decentralization</b>. Stablecoins are centralized because they are pegged to a fiat price formed in international markets</b>.</p>
 									</div>
 
 									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab3" role="tabpanel" aria-labelledby="v-pills-backed-tab">
 										<h4 className="font-italic mb-4">Decentralized / Interoperable</h4>
 										<hr/>
 										<p className="font-italic text-muted mb-2">Catallactic builts on top of the existing DLT networks. Despite more work is need, we consider there is already a foundation that can be used for our purposes.</p>
-										<p className="font-italic text-muted mb-2">We think there is not yet any network which is good enough and the overall status of the industry is in beta status and not yet ready for massification. As a consequence <p>we do not adhere to specific chains</p>. Instead, we build for any chains which can process transactionS on top of the <p>EVM</p>, as the more used standard for smart contracts implementation. We consider in future to adapt to other virtual machines and languages.</p>
-										<p className="font-italic text-muted mb-2">Catallactic has <p>preference for PoW chains</p>. <p>EVM-enabled Bitcoin L2s</p> able being explored as ideal hosts for Catallactic smart contracts.</p>
+										<p className="font-italic text-muted mb-2">We think there is not yet any network which is good enough and the overall status of the industry is in beta status and not yet ready for massification. As a consequence <b>we do not adhere to specific chains</b>. Instead, we build for any chains which can process transactionS on top of the <b>EVM</b>, as the more used standard for smart contracts implementation. We consider in future to adapt to other virtual machines and languages.</p>
+										<p className="font-italic text-muted mb-2">Catallactic has <b>preference for PoW chains</b>. <b>EVM-enabled Bitcoin L2s</b> able being explored as ideal hosts for Catallactic smart contracts.</p>
 										<p className="font-italic text-muted mb-2">We understand that different necessities can require different trust levels. Therefore Catallactic can also be operated on PoS networks with higher traction or number of users.</p>
-										<p className="font-italic text-muted mb-2">Finally, Catallactic is thought with <p>interoperability</p> principles in mind, allowing distribution of the liquidity across different chains, as defined by the currency issuer.</p>
+										<p className="font-italic text-muted mb-2">Finally, Catallactic is thought with <b>interoperability</b> principles in mind, allowing distribution of the liquidity across different chains, as defined by the currency issuer.</p>
 									</div>
 
 									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab4" role="tabpanel" aria-labelledby="v-pills-profile-tab">
 										<h4 className="font-italic mb-4">Ready for Issuers</h4>
 										<hr/>
 										<p className="font-italic text-muted mb-2">Catallactic aims to relief CryptoCommodity issuers of the burden to define principles, acquire know-how, develop tools and design processes to issue tokens backed by the value of their underlying assets. Instead, we create an opinionated system, but maintain configurability, which allows cryptocurrency issuers focusing on their core business.</p>
-										<p className="font-italic text-muted mb-2">We deliver a <p>Tokenization Platform</p> which allows adopters to issue and manage their CryptoCommodity without the technical burden.</p>
+										<p className="font-italic text-muted mb-2">We deliver a <b>Tokenization Platform</b> which allows adopters to issue and manage their CryptoCommodity without the technical burden.</p>
 										<p className="font-italic text-muted mb-2">The outcomes from Catallactic are Open Source and will allow private actors to build additional tools, plugins or know-how that can complement Catallactic deliverables.</p>
 									</div>
 
 									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab5" role="tabpanel" aria-labelledby="v-pills-local-tab">
 										<h4 className="font-italic mb-4">Local Scope / In Competition</h4>
 										<hr/>
-										<p className="font-italic text-muted mb-2">Different communities have different monetary requirements and commodities availability. To enable this fine tuning of the monetary availability, we think CryptoCommodities should be issued and applicable at <p>local scope by local actors</p>. This provides a valuable tool for communities driven entrepreneurship.</p>
-										<p className="font-italic text-muted mb-2">A <p>CryptoCommodity algebra</p> allows technical support for real world use cases as merges and acquisitions or CryptoCommodity splits.</p>
-										<p className="font-italic text-muted mb-2">Single currencies as Bitcoin represent a single point of failure and a evident target for scammers and censonship. To deliver censorship resistance and suitAble price formation, CryptoCommodities are thought to be <p>issued by multiple actors and in full competition</p>, relying on the market the price formation task.</p>
+										<p className="font-italic text-muted mb-2">Different communities have different monetary requirements and commodities availability. To enable this fine tuning of the monetary availability, we think CryptoCommodities should be issued and applicable at <b>local scope by local actors</b>. This provides a valuable tool for communities driven entrepreneurship.</p>
+										<p className="font-italic text-muted mb-2">A <b>CryptoCommodity algebra</b> allows technical support for real world use cases as merges and acquisitions or CryptoCommodity splits.</p>
+										<p className="font-italic text-muted mb-2">Single currencies as Bitcoin represent a single point of failure and a evident target for scammers and censonship. To deliver censorship resistance and suitAble price formation, CryptoCommodities are thought to be <b>issued by multiple actors and in full competition</b>, relying on the market the price formation task.</p>
 									</div>
 
                 </div>
