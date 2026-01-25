3bdca2600526bd918a9871a07c089c6bf347a907
pellyadolfo <pellyadolfo@yahoo.es>
2026-01-25 12:05:46 +0100

    removed technical

--- src/components/sections/WhySection/index.tsx
+++ src/components/sections/WhySection/index.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.css';
 
 function setShownTab(num: number) {
 
-	for (let i = 0; i <= 5; i++) {
+	for (let i = 0; i <= 4; i++) {
 		document.getElementById("v-whytab" + i).classList.remove("active");
 		document.getElementById("whytab" + i).classList.remove("show");
 		document.getElementById("whytab" + i).classList.remove("active");
@@ -43,15 +43,11 @@ export default function WhySection(): JSX.Element {
 									<span className="font-weight-bold text-uppercase">Stable in Value / Non-Fiat</span>
 								</a>
 
-								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab3" data-toggle="pill" href="#v-pills-stable" role="tab" aria-controls="v-pills-stable" aria-selected="false" onClick={() => setShownTab(3)}>
-									<span className="font-weight-bold text-uppercase">Decentralized / Interoperable</span>
-								</a>
-
-								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab4" data-toggle="pill" href="#v-pills-ready" role="tab" aria-controls="v-pills-ready" aria-selected="false" onClick={() => setShownTab(4)}>
+								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab3" data-toggle="pill" href="#v-pills-ready" role="tab" aria-controls="v-pills-ready" aria-selected="false" onClick={() => setShownTab(3)}>
 									<span className="font-weight-bold text-uppercase">Ready to Use for Issuers</span>
 								</a>
 
-								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab5" data-toggle="pill" href="#v-pills-competition" role="tab" aria-controls="v-pills-competition" aria-selected="false" onClick={() => setShownTab(5)}>
+								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab4" data-toggle="pill" href="#v-pills-competition" role="tab" aria-controls="v-pills-competition" aria-selected="false" onClick={() => setShownTab(4)}>
 									<span className="font-weight-bold text-uppercase">Local Competition</span>
 								</a>
 
@@ -87,17 +83,7 @@ export default function WhySection(): JSX.Element {
 										<p className="font-italic text-muted mb-2"><b>Catallactic advocates for a <b>built-in price formation</b> as a basis to deliver true <b>value decentralization</b>. Stablecoins are centralized because they are pegged to a fiat price formed in international markets</b>.</p>
 									</div>
 
-									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab3" role="tabpanel" aria-labelledby="v-pills-backed-tab">
-										<h4 className="font-italic mb-4">Decentralized / Interoperable</h4>
-										<hr/>
-										<p className="font-italic text-muted mb-2">Catallactic builts on top of the existing DLT networks. Despite more work is need, we consider there is already a foundation that can be used for our purposes.</p>
-										<p className="font-italic text-muted mb-2">We think there is not yet any network which is good enough and the overall status of the industry is in beta status and not yet ready for massification. As a consequence <b>we do not adhere to specific chains</b>. Instead, we build for any chains which can process transactionS on top of the <b>EVM</b>, as the more used standard for smart contracts implementation. We consider in future to adapt to other virtual machines and languages.</p>
-										<p className="font-italic text-muted mb-2">Catallactic has <b>preference for PoW chains</b>. <b>EVM-enabled Bitcoin L2s</b> able being explored as ideal hosts for Catallactic smart contracts.</p>
-										<p className="font-italic text-muted mb-2">We understand that different necessities can require different trust levels. Therefore Catallactic can also be operated on PoS networks with higher traction or number of users.</p>
-										<p className="font-italic text-muted mb-2">Finally, Catallactic is thought with <b>interoperability</b> principles in mind, allowing distribution of the liquidity across different chains, as defined by the currency issuer.</p>
-									</div>
-
-									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab4" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab3" role="tabpanel" aria-labelledby="v-pills-profile-tab">
 										<h4 className="font-italic mb-4">Ready to Use for Issuers</h4>
 										<hr/>
 										<p className="font-italic text-muted mb-2">Catallactic aims to relief CryptoCommodity issuers of the burden to define principles, acquire know-how, develop tools and design processes to issue tokens backed by the value of their underlying assets. Instead, we create an opinionated system, but maintain configurability, which allows cryptocurrency issuers focusing on their core business.</p>
@@ -105,7 +91,7 @@ export default function WhySection(): JSX.Element {
 										<p className="font-italic text-muted mb-2">The outcomes from Catallactic are Open Source and will allow private actors to build additional tools, plugins or know-how that can complement Catallactic deliverables.</p>
 									</div>
 
-									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab5" role="tabpanel" aria-labelledby="v-pills-local-tab">
+									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab4" role="tabpanel" aria-labelledby="v-pills-local-tab">
 										<h4 className="font-italic mb-4">Local Competition</h4>
 										<hr/>
 										<p className="font-italic text-muted mb-2">Different communities have different monetary requirements and commodities availability. To enable fine tuning of the monetary availability, CryptoCommodities should be issued and applicable at <b>local scope by local actors</b>. This provides a valuable tool for community driven entrepreneurship.</p>
