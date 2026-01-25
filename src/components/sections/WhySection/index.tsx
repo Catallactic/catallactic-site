26922b98b95932eb83d09430784908df2588ec68
pellyadolfo <pellyadolfo@yahoo.es>
2026-01-25 15:15:21 +0100

    removed text

--- src/components/sections/WhySection/index.tsx
+++ src/components/sections/WhySection/index.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.css';
 
 function setShownTab(num: number) {
 
-	for (let i = 0; i <= 4; i++) {
+	for (let i = 0; i <= 3; i++) {
 		document.getElementById("v-whytab" + i).classList.remove("active");
 		document.getElementById("whytab" + i).classList.remove("show");
 		document.getElementById("whytab" + i).classList.remove("active");
@@ -31,23 +31,19 @@ export default function WhySection(): JSX.Element {
             <div className="col-md-3">
 							<div className="nav flex-column nav-pills nav-pills-custom" id="v-pills-tab" role="tablist" aria-orientation="vertical">
 
-								<a className="pp nav-link mb-3 p-3 shadow active" id="v-whytab0" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true" onClick={() => setShownTab(0)}>
-									<span className="font-weight-bold text-uppercase">Why Catallactic</span>
-								</a>
-
-								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab1" data-toggle="pill" href="#v-pills-backed" role="tab" aria-controls="v-pills-backed" aria-selected="false" onClick={() => setShownTab(1)}>
+								<a className="pp nav-link mb-3 p-3 shadow active" id="v-whytab0" data-toggle="pill" href="#v-pills-backed" role="tab" aria-controls="v-pills-backed" aria-selected="false" onClick={() => setShownTab(0)}>
 									<span className="font-weight-bold text-uppercase">Backed By Commodities</span>
 								</a>
 
-								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab2" data-toggle="pill" href="#v-pills-stable" role="tab" aria-controls="v-pills-stable" aria-selected="false" onClick={() => setShownTab(2)}>
+								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab1" data-toggle="pill" href="#v-pills-stable" role="tab" aria-controls="v-pills-stable" aria-selected="false" onClick={() => setShownTab(1)}>
 									<span className="font-weight-bold text-uppercase">Stable in Value / Non-Fiat</span>
 								</a>
 
-								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab3" data-toggle="pill" href="#v-pills-ready" role="tab" aria-controls="v-pills-ready" aria-selected="false" onClick={() => setShownTab(3)}>
+								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab2" data-toggle="pill" href="#v-pills-ready" role="tab" aria-controls="v-pills-ready" aria-selected="false" onClick={() => setShownTab(2)}>
 									<span className="font-weight-bold text-uppercase">Ready to Use for Issuers</span>
 								</a>
 
-								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab4" data-toggle="pill" href="#v-pills-competition" role="tab" aria-controls="v-pills-competition" aria-selected="false" onClick={() => setShownTab(4)}>
+								<a className="pp nav-link mb-3 p-3 shadow" id="v-whytab3" data-toggle="pill" href="#v-pills-competition" role="tab" aria-controls="v-pills-competition" aria-selected="false" onClick={() => setShownTab(3)}>
 									<span className="font-weight-bold text-uppercase">Local Competition</span>
 								</a>
 
@@ -57,17 +53,7 @@ export default function WhySection(): JSX.Element {
             <div className="col-md-9">
                 <div className="tab-content" id="v-pills-tabContent">
 
-									<div className="tab-pane fade shadow rounded bg-white p-5 show active" id="whytab0" role="tabpanel" aria-labelledby="v-pills-home-tab">
-										<h4 className="font-italic mb-4">Why Catallactic?</h4>
-										<hr/>
-										<p className="font-italic text-muted mb-2">Because this is the real crypto.</p>
-										<p className="font-italic text-muted mb-2">Crypto was born in 2009 with the promise of decentralization from a non fiat currency, Bitcoin. However, tokenization projects have evolved with the financing of venture-capital backed crypto companies and financial firms, that have modified the initial promise, to make it more centralized and fiat dependent.</p>
-										<p className="font-italic text-muted mb-2">Whereas we think this is a distortion from the initial principles, we find that the tools and processs developed inherit this centraliced and fiat nature and alternative approaches are miss-represented. In short there is a fiat bias in tools and processes.</p>
-										<p className="font-italic text-muted mb-2">Catallactic aims to define the principles, know-how, tools and processes to tokenize, fund and manage decentralized commodity based tokens and relief the issuer from the technological and operational burden.</p>
-										<p className="font-italic text-muted mb-2">We break with the Quantitative Theory of Money, which assumes a monopolistic legal tender and a central planner, to build a <b>Qualitative Theory of Money</b> for competing currencies privately issued.</p>
-									</div>
-
-									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab1" role="tabpanel" aria-labelledby="v-pills-competition-tab">
+									<div className="tab-pane fade shadow rounded bg-white p-5 show active" id="whytab0" role="tabpanel" aria-labelledby="v-pills-competition-tab">
 										<h4 className="font-italic mb-4">Backed By Commodities</h4>
 										<hr/>
 										<p className="font-italic text-muted mb-2">Catallactic aims to build cryptocurrencies that comply with <b>Regression Theorem</b>, and therefore, that capture the intrinsic value of Commodities as underlaying asset. This is why the currencies are called as <b>CryptoCommodities</b>.</p>
@@ -75,7 +61,7 @@ export default function WhySection(): JSX.Element {
 										<p className="font-italic text-muted mb-2">Different Commodities, issuers and scopes will have different features, so there is a degree of <b>customization</b> for the dimensions of every CryptoCommodity.</p>
 									</div>
 
-									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab2" role="tabpanel" aria-labelledby="v-pills-backed-tab">
+									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab1" role="tabpanel" aria-labelledby="v-pills-backed-tab">
 										<h4 className="font-italic mb-4">Stable in Value / Non-Fiat</h4>
 										<hr/>
 										<p className="font-italic text-muted mb-2">By exposing commodities intrinsic value enables capturing into the token price the <b>marginal utility as perceived by consumers</b>. Differently to fiat stablecoins, where the price is captured from an external market, CryptoCommodities allow a natural price formation performed by a required underlying market of the commodity. This guarantees <b>no relative price distortion other than consumers will</b>.</p>
@@ -83,7 +69,7 @@ export default function WhySection(): JSX.Element {
 										<p className="font-italic text-muted mb-2"><b>Catallactic advocates for a <b>built-in price formation</b> as a basis to deliver true <b>value decentralization</b>. Stablecoins are centralized because they are pegged to a fiat price formed in international markets</b>.</p>
 									</div>
 
-									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab3" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab2" role="tabpanel" aria-labelledby="v-pills-profile-tab">
 										<h4 className="font-italic mb-4">Ready to Use for Issuers</h4>
 										<hr/>
 										<p className="font-italic text-muted mb-2">Catallactic aims to relief CryptoCommodity issuers of the burden to define principles, acquire know-how, develop tools and design processes to issue tokens backed by the value of their underlying assets. Instead, we create an opinionated system, but maintain configurability, which allows cryptocurrency issuers focusing on their core business.</p>
@@ -91,7 +77,7 @@ export default function WhySection(): JSX.Element {
 										<p className="font-italic text-muted mb-2">The outcomes from Catallactic are Open Source and will allow private actors to build additional tools, plugins or know-how that can complement Catallactic deliverables.</p>
 									</div>
 
-									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab4" role="tabpanel" aria-labelledby="v-pills-local-tab">
+									<div className="tab-pane fade shadow rounded bg-white p-5" id="whytab3" role="tabpanel" aria-labelledby="v-pills-local-tab">
 										<h4 className="font-italic mb-4">Local Competition</h4>
 										<hr/>
 										<p className="font-italic text-muted mb-2">Different communities have different monetary requirements and commodities availability. To enable fine tuning of the monetary availability, CryptoCommodities should be issued and applicable at <b>local scope by local actors</b>. This provides a valuable tool for community driven entrepreneurship.</p>
